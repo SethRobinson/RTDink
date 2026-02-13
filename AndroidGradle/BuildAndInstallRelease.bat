b1a84d9897ffc17e2cde16d607eb85d6ac8248da
Seth Robinson <seth@rtsoft.com>
2026-02-13 15:54:16 +0900

    * Fixed IOS build (requires Proton github update too), and added a helper .bat file for building the android release for testing from Windows

---
+++ AndroidGradle/BuildAndInstallRelease.bat
@@ -0,0 +1,13 @@
+echo Set JAVA_HOME and other vars (hope you edited ../../base_setup.bat)
+cd ..
+call app_info_setup.bat
+cd AndroidGradle
+
+#to force a full rebuild
+call gradlew clean
+
+:First uninstall any debug builds, otherwise this will fail
+call gradlew uninstallDebug
+call gradlew installRelease
+adb shell monkey -p com.rtsoft.rtdink -c android.intent.category.LAUNCHER 1
+pause
